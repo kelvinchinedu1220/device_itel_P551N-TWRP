f3060e8da1d6846a8265dbd7688d22b7086b2567
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-08-27 23:53:09 +0100

    Update twrp_P661N.mk

--- twrp_P661N.mk
+++ twrp_P661N.mk
@@ -4,17 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from P661n device
-$(call inherit-product, device/itel/P661n/device.mk)
+# Inherit from P661N device
+$(call inherit-product, device/itel/P661N/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := twrp_P661n
-PRODUCT_DEVICE := P661n
+PRODUCT_NAME := twrp_P661N
+PRODUCT_DEVICE := P661N
 PRODUCT_BRAND := Itel
-PRODUCT_MODEL := Itel P661n
+PRODUCT_MODEL := Itel P661N
 PRODUCT_MANUFACTURER := itel
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
